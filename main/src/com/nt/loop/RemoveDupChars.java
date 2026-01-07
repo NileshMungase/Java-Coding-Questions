801bc1d5555be9d0a9757aef2001b1b63ef75335
Nilesh Mungase <nileshmungase101@gmail.com>
2026-01-07 11:26:31 +0530

    commit

--- main/src/com/nt/loop/RemoveDupChars.java
+++ main/src/com/nt/loop/RemoveDupChars.java
@@ -9,5 +9,21 @@ public class RemoveDupChars {
         StringBuilder sb1 = new StringBuilder();
         str.chars().distinct().forEach(c -> sb1.append((char) c));
         System.out.println(sb1);
+        
+        //Approach -2 
+        
+        StringBuilder sb2 = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int idx = str.indexOf(ch, i + 1);
+            if(idx==-1)
+            {
+                sb2.append(ch);
+            }
+        }
+        System.out.println(sb2);
+
+        //Approach 3
+        
     }
 }
