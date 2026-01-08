bf71c41f53350d0f6c6b131947ed324cb5e8b9b7
NileshMungase <nileshmungase101@gmail.com>
2026-01-08 12:20:29 +0530

    commit

---
+++ main/src/com/nt/loop/OccrOfEachChar.java
@@ -0,0 +1,25 @@
+package com.nt.loop;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OccrOfEachChar {
+    static void main(String[] args) {
+        String input = "hello";
+        Map<Character, Integer> map = new HashMap<>();
+        char[] chars = input.toCharArray();
+        for(char ch : chars)
+        {
+            if(!map.containsKey(ch))
+            {
+                map.put(ch, 1);
+            }
+            else
+            {
+                int value = map.get(ch);
+                Integer put = map.put(ch, value+1);
+            }
+        }
+        System.out.println(map);
+    }
+}
