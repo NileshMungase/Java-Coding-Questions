333359b522fae4d293eab2084c059a2543019cf1
NileshMungase <nileshmungase101@gmail.com>
2026-01-09 08:14:21 +0530

    commit

---
+++ main/src/com/nt/loop/MyInfo.java
@@ -0,0 +1,30 @@
+package com.nt.programs;
+
+public class MyInfo {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		String name = "Nilesh";
+		int age = 22;
+		long phoneNo = 7447847064L;
+		float demoval = 45.45F;
+		double demoVal = 45.12345D;
+		char demoChar = 'N';
+
+		/*
+		 * System.out.println("Name : "+name); System.out.println("Age : "+age);
+		 * System.out.println("Phone Number : "+phoneNo);
+		 * System.out.println("demoval : "+demoval);
+		 * System.out.println("Demo Value : "+demoVal);
+		 * System.out.println("Demo Character : "+demoChar);
+		 */
+
+		/*
+		 * System.out.println("Name : " + name + " Age : " + age + " Phone Number : " +
+		 * phoneNo + " demo value : " + demoval + " Demo Value : " + demoVal +
+		 * " Demo Character : " + demoChar);
+		 */
+
+	}
+
+}
