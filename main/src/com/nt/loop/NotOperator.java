3c074111518b2adfe7288ac948a12a8e9c754a27
NileshMungase <nileshmungase101@gmail.com>
2026-01-09 08:15:12 +0530

    commit

---
+++ main/src/com/nt/loop/NotOperator.java
@@ -0,0 +1,14 @@
+package com.nt.programs;
+
+public class NotOperator {
+
+	public static void main(String[] args) {
+		String userType = "Admin";
+		if (userType != "Admin") {
+			System.out.println("You have a access. ");
+		} else {
+			System.out.println("You have not access. ");
+		}
+	}
+
+}
