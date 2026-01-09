1748935a0d979e7339559bacdc71efea1176641a
NileshMungase <nileshmungase101@gmail.com>
2026-01-09 08:15:58 +0530

    commit

---
+++ main/src/com/nt/loop/SecondProgram.java
@@ -0,0 +1,15 @@
+package com.nt.programs;
+
+public class SecondProgram {
+	public static void demo(String name)
+	{
+		System.out.println(name);
+	}
+
+	public static void main(String[] args) {
+		String name = "Demo";
+		SecondProgram.demo(name);
+		
+	}
+
+}
