d52c1543068bb75b6be1dba44b5be11d89efe490
NileshMungase <nileshmungase101@gmail.com>
2026-01-10 21:44:04 +0530

    commit

---
+++ main/src/com/nt/string/CountCharacters.java
@@ -0,0 +1,9 @@
+package com.nt.string;
+
+public class CountCharacters {
+    public static void main(String[] args) {
+        String str = "Nilesh";
+        System.out.println(str.length());
+    }
+}
+
