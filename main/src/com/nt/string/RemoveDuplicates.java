d63755113fdf23b8d0e0074e5c49bbd77351d488
NileshMungase <nileshmungase101@gmail.com>
2026-01-11 22:24:31 +0530

    commit

---
+++ main/src/com/nt/string/RemoveDuplicates.java
@@ -0,0 +1,14 @@
+package com.nt.string;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        String str = "aabbcc";
+        String result = "";
+
+        for (char ch : str.toCharArray()) {
+            if (!result.contains(String.valueOf(ch)))
+                result += ch;
+        }
+        System.out.println(result);
+    }
+}
