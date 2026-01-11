993bcdf9bfa68807a79d35e7b6c691ef8d687cb7
NileshMungase <nileshmungase101@gmail.com>
2026-01-11 22:27:57 +0530

    commit

---
+++ main/src/com/nt/string/SwapString.java
@@ -0,0 +1,14 @@
+package com.nt.string;
+
+public class SwapString {
+    public static void main(String[] args) {
+        String a = "Mungase";
+        String b = "Nilesh";
+
+        a = a + b;
+        b = a.substring(0, a.length() - b.length());
+        a = a.substring(b.length());
+
+        System.out.println(a + " " + b);
+    }
+}
